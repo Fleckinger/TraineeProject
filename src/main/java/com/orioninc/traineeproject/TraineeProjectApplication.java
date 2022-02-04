bcf2d921d9fbbfacd049226f1adb1104fec11ecb
Vladislav Safarov <vladislav.safarov@mera.com>
2022-02-04 17:14:20 +0300

    initial commit

---
+++ src/main/java/com/orioninc/traineeproject/TraineeProjectApplication.java
@@ -0,0 +1,13 @@
+package com.orioninc.traineeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraineeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TraineeProjectApplication.class, args);
+    }
+
+}
